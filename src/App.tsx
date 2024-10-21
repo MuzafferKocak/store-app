9ef7d7f37fb595d6931777b35fc52a639b6f3fb2
Mahir <mkojak75@gmail.com>
2024-10-22 00:52:38 +0200

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPages from "./pages/FavoritesPages";
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<FavoritesPages />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
