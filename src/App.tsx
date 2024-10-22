bc16c92ef0b43f87d0e93147f9c64951e8cb66c0
Mahir <mkojak75@gmail.com>
2024-10-23 00:23:49 +0200

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritesPages from "./pages/FavoritesPages";
 import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
     <div className="bg-green-200 min-h-screen">
+      <Provider store={store}>
       <BrowserRouter>
       <Navbar/>
         <Routes>
@@ -14,6 +17,8 @@ function App() {
           <Route path="/favorites" element={<FavoritesPages />}></Route>
         </Routes>
       </BrowserRouter>
+      </Provider>
+      
     </div>
   );
 }
