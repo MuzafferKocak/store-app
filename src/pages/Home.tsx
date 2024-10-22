83d66f1b026e90ac0962c291a95941f5eaf8c444
Mahir <mkojak75@gmail.com>
2024-10-23 00:55:36 +0200

    axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import SearchComp from "../components/SearchComp";
+import axios from "axios";
+import { useAppDispatch } from "../app/hooks";
+import { fetchFail, fetchStart, getSuccessProduct } from "../features/productsSlice";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+export interface Products {
+  products: Products[];
+  total: number;
+  skip: number;
+  limit: number;
 }
 
-export default Home
+const Home = () => {
+  const [search, setSearch] = useState("");
+  const dispatch = useAppDispatch()
+
+  const getData = async () => {
+    dispatch(fetchStart())
+    try {
+      const { data } = await axios.get<Products>(
+        `https://dummyjson.com/products/search?q=${search}`
+        
+      );
+      console.log(data.products);
+      dispatch(getSuccessProduct(data.products))
+    } catch (error) {
+      dispatch(fetchFail())
+    }
+  };
+
+  useEffect(()=>{
+    getData()
+  },[search])
+
+  return (
+    <div>
+      <SearchComp />
+    </div>
+  );
+};
+
+export default Home;
