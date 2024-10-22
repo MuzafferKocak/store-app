ef1452a6c907ea074e222b4e66d224c4f998864f
Mahir <mkojak75@gmail.com>
2024-10-22 23:36:57 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritesPages from "./pages/FavoritesPages";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-green-200 min-h-screen">
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/favorites" element={<FavoritesPages />}></Route>
