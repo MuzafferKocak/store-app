97b7575316c93690f314690926960f4a2b82cc91
Mahir <mkojak75@gmail.com>
2024-10-24 00:03:05 +0200

    slice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,77 @@
 import React, { useEffect, useState } from "react";
 import SearchComp from "../components/SearchComp";
 import axios from "axios";
-import { useAppDispatch } from "../app/hooks";
-import { fetchFail, fetchStart, getSuccessProduct } from "../features/productsSlice";
-import { EventFunc } from "../models/models";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  fetchFail,
+  fetchStart,
+  getSuccessProduct,
+} from "../features/productsSlice";
+import { EventFunc, Products } from "../models/models";
 
-export interface Products {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
-}
+//* model.ts Global
+// export interface Products {
+//   products: Product[];
+//   total: number;
+//   skip: number;
+//   limit: number;
+// }
 
 const Home = () => {
   const [search, setSearch] = useState("");
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const { loading, error, productsList } = useAppSelector(
+    (state) => state.products
+  );
 
   const getData = async () => {
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       const { data } = await axios.get<Products>(
         `https://dummyjson.com/products/search?q=${search}`
-        
       );
       console.log(data.products);
-      dispatch(getSuccessProduct(data.products))
+      dispatch(getSuccessProduct(data.products));
     } catch (error) {
-      dispatch(fetchFail())
+      dispatch(fetchFail());
     }
   };
 
-  useEffect(()=>{
-    getData()
-  },[search])
+  useEffect(() => {
+    getData();
+  }, [search]);
 
   // const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
   //  setSearch(e.target.value)
 
   // }
 
-  const handleChange:EventFunc = (e)=>{
-    setSearch(e.target.value)
-  }
+  const handleChange: EventFunc = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div>
       <SearchComp handleChange={handleChange} />
+      {loading ? (
+        <div className="mt-32">
+          <p className="text-center font-bold text-red-600">
+            Products loading...!
+          </p>
+        </div>
+      ) : error ? (
+        <div className="mt-32">
+          <p className="text-center font-bold text-red-600">
+            Someting went loading...!
+          </p>
+        </div>
+      ) : (
+        <div>
+          {productsList.map((item) => (
+            <p>{item.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
